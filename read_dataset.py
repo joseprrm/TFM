9113e4780e5a574c98d43d8d2a7db8041962c5dd
Josep Ribes <jrrm@tutanota.com>
2024-10-24 00:21:03 +0200

    add comments

--- read_dataset.py
+++ read_dataset.py
@@ -19,9 +19,35 @@ def read_multiple_csv(paths, dataset_metadata):
     return dataframe
 
 def find_partition_index(index_rows, row):
+    """
+    Find the partition index in the index data structure
+
+    index_rows: list of last row in each partition
+    row: row that the caller wants the partition index
+
+    returns: the number of the partition
+    """
     return utils.first_true_element([row < i for i in index_rows])
 
 def find_partition_paths(index, first_row, last_row):
+    """
+    index: index data structure
+    first_row: first row in the range
+    last_row: last row in the range (exclusive range)
+
+    returns:
+        paths: list of the paths of the partitions
+        start: first row in the partition group
+        end: last row in the partition group (exclusive range)
+
+    Index data structure:
+        The index data structure is a dict, but is not used as such.
+        keys() hold the "last row + 1" of each partition.
+        values() hold the path of each partition file.
+    """
+    # variables with index_<> refer to index as a the whole "indexing data structure" (index)
+    # variables with <>_index refer to the actual index number (an integer) in the "indexing data structure" (index)
+
     index_rows = list(index.keys())
     index_paths = list(index.values())
 
@@ -30,7 +56,11 @@ def find_partition_paths(index, first_row, last_row):
 
     # +1 because we also want the last partition 
     paths = [index_paths[i] for i in range(first_row_index, last_row_index + 1)]
+
+    # start is 0 if first partition is the first of the dataset
+    # otherwise, the start is the previous partition "row" (the one that determines the partition in the index)
     start = 0 if first_row_index == 0 else index_rows[first_row_index - 1]
+    # end is the partition "row"
     end = index_rows[last_row_index]
 
     return paths, start, end
