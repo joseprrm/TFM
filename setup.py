63b686d8f0ce9090a5165403946c21c64fb5d354
Josep Ribes <jrrm@tutanota.com>
2024-11-06 19:16:28 +0100

    Update requirements and setup

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
 
+def parse_requirements():
+    with open('requirements.txt', 'rt') as f:
+        return f.readlines()
+
 setup(
     name="tfm",
     version="0.1",
     packages=find_packages(where="src"),
-    package_dir={"": "src"}
+    package_dir={"": "src"},
+    install_requires=parse_requirements()
 )
